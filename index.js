c9198bb00b380030b0f80c1fdf785d301c45459e
Bach Le <lehoangbach7802@gmail.com>
2022-05-18 10:10:12 -0400

    commit tree f23e893ac5c79c085c2836f7273c4511686aa89f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+customerName;
+
+// const upperCaseCustomerName = function () {customerName.toUpperCase;}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bach';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'not bach';
+}
